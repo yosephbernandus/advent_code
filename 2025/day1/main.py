94ecf7fcfc092d93289d172878a1d5a93b18610e
yosephbernandus <yosephbernandus@gmail.com>
2025-12-02 21:05:18 +0700

    2025 day1 part 2 python

--- 2025/day1/main.py
+++ 2025/day1/main.py
@@ -1,3 +1,25 @@
+# with open("input.txt", "r") as f:
+#     lines = [line.strip() for line in f]
+#
+# position = 50
+# password = 0
+#
+# for line in lines:
+#     direction = line[0]
+#     distance = int(line[1:])
+#
+#     if direction == "L":
+#         position = (position - distance) % 100
+#     else:
+#         position = (position + distance) % 100
+#
+#     if position == 0:
+#         password += 1
+#
+# print(password)
+
+
+# Part 2
 with open("input.txt", "r") as f:
     lines = [line.strip() for line in f]
 
@@ -9,11 +31,14 @@ for line in lines:
     distance = int(line[1:])
 
     if direction == "L":
+        if position == 0:
+            password += distance // 100
+        else:
+            if distance >= position:
+                password += 1 + (distance - position) // 100
         position = (position - distance) % 100
-    else:
+    else:  # R
+        password += (position + distance) // 100
         position = (position + distance) % 100
-
-    if position == 0:
-        password += 1
 
 print(password)
